43e14e8bb4d02cef53f92175f02e8ace261277e0
Yury Fedotov <yury_fedotov@mckinsey.com>
2024-05-30 16:26:21 -0400

    Develop Map.visualize() API

--- src/ticket_to_ride/map.py
+++ src/ticket_to_ride/map.py
@@ -7,9 +7,12 @@
 # the official Days of Wonder website: https://www.daysofwonder.com/ticket-to-ride/.
 """Map machinery to represent game board."""
 import typing as tp
+from collections import defaultdict
 
+import matplotlib.pyplot as plt
 import networkx as nx
 
+from .color import Color
 from .route import Route
 
 
@@ -26,6 +29,50 @@ class Map:
         for route in routes:
             route.add_as_edge(self.graph)
         self._check_has_no_bridges()
+
+    def visualize(self: tp.Self) -> None:
+        """Visualize self as a graph figure similar to actual game board."""
+        plt.figure(figsize=(12, 12))
+
+        # Resolve positions.
+        pos = nx.spring_layout(self.graph, seed=42)
+
+        # Draw nodes.
+        nx.draw_networkx_nodes(self.graph, pos, node_size=1200, node_color="#FFF", edgecolors="black")
+        nx.draw_networkx_labels(
+            self.graph,
+            pos,
+            labels={n: n.name for n in self.graph},
+            font_size=7,
+            font_family="sans-serif",
+            font_weight="bold",
+        )
+
+        # Draw edges.
+        edges_by_color = self._get_edges_by_color()
+        for color, edges in edges_by_color.items():
+            nx.draw_networkx_edges(
+                self.graph,
+                pos,
+                edgelist=edges,
+                width=2,
+                alpha=0.7,
+                edge_color=color.value,
+                style="dashed",
+            )
+        edge_labels = nx.get_edge_attributes(self.graph, name="length")
+        nx.draw_networkx_edge_labels(self.graph, pos, edge_labels)
+
+        plt.axis("off")
+        plt.tight_layout()
+        plt.show()
+
+    def _get_edges_by_color(self: tp.Self) -> tp.Dict[Color, tp.List[tp.Any]]:
+        edges_by_color: tp.Dict[Color, tp.List[tp.Any]] = defaultdict(list)
+        for (u, v, ddict) in self.graph.edges(data=True):
+            color: Color = ddict["color"]
+            edges_by_color[color].append((u, v))  # TODO: persist all edge info?
+        return edges_by_color  # TODO: make values tuples for safety?
 
     def _check_has_no_bridges(self: tp.Self) -> None:
         bridges = frozenset(nx.bridges(self.graph))
