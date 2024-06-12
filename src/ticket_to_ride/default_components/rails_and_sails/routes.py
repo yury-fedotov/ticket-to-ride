d1bdb16e53d1503e572bbdfb26a0de78be9b1d83
Yury Fedotov <yury_fedotov@mckinsey.com>
2024-06-11 23:39:05 -0400

    Add all Eurasia routes

--- src/ticket_to_ride/default_components/rails_and_sails/routes.py
+++ src/ticket_to_ride/default_components/rails_and_sails/routes.py
@@ -83,4 +83,100 @@ default_routes = (
         cities=(default_cities["Manila"], default_cities["Tokyo"]),
         length=2, color=Color.YELLOW, transportation_type=TransportationType.SHIP,
     ),
+    Route(
+        cities=(default_cities["Manila"], default_cities["Bangkok"]),
+        length=2, color=Color.RED, transportation_type=TransportationType.SHIP,
+    ),
+    Route(
+        cities=(default_cities["Manila"], default_cities["Jakarta"]),
+        length=2, color=Color.NEUTRAL, transportation_type=TransportationType.SHIP,
+    ),
+    Route(
+        cities=(default_cities["Jakarta"], default_cities["Bangkok"]),
+        length=2, color=Color.GREEN, transportation_type=TransportationType.SHIP,
+    ),
+    Route(
+        cities=(default_cities["Bangkok"], default_cities["Mumbai"]),
+        length=3, color=Color.RED, transportation_type=TransportationType.TRAIN,
+    ),
+    Route(
+        cities=(default_cities["Bangkok"], default_cities["Mumbai"]),
+        length=3, color=Color.YELLOW, transportation_type=TransportationType.TRAIN,
+    ),
+    Route(
+        cities=(default_cities["Mumbai"], default_cities["Labore"]),
+        length=1, color=Color.GREEN, transportation_type=TransportationType.TRAIN,
+    ),
+    Route(
+        cities=(default_cities["Mumbai"], default_cities["Labore"]),
+        length=1, color=Color.NEUTRAL, transportation_type=TransportationType.TRAIN,
+    ),
+    Route(
+        cities=(default_cities["Beijing"], default_cities["Labore"]),
+        length=3, color=Color.NEUTRAL, transportation_type=TransportationType.TRAIN,
+    ),
+    Route(
+        cities=(default_cities["Novosibirsk"], default_cities["Labore"]),
+        length=2, color=Color.WHITE, transportation_type=TransportationType.TRAIN,
+    ),
+    Route(
+        cities=(default_cities["Labore"], default_cities["Tehran"]),
+        length=2, color=Color.NEUTRAL, transportation_type=TransportationType.TRAIN,
+    ),
+    Route(
+        cities=(default_cities["Mumbai"], default_cities["Tehran"]),
+        length=3, color=Color.PINK, transportation_type=TransportationType.TRAIN,
+    ),
+    Route(
+        cities=(default_cities["Mumbai"], default_cities["Tehran"]),
+        length=3, color=Color.WHITE, transportation_type=TransportationType.TRAIN,
+    ),
+    Route(
+        cities=(default_cities["Moskva"], default_cities["Tehran"]),
+        length=3, color=Color.NEUTRAL, transportation_type=TransportationType.TRAIN,
+    ),
+    Route(
+        cities=(default_cities["Tehran"], default_cities["Athina"]),
+        length=2, color=Color.NEUTRAL, transportation_type=TransportationType.TRAIN,
+    ),
+    Route(
+        cities=(default_cities["Athina"], default_cities["Hamburg"]),
+        length=2, color=Color.GREEN, transportation_type=TransportationType.TRAIN,
+    ),
+    Route(
+        cities=(default_cities["Athina"], default_cities["Marseille"]),
+        length=2, color=Color.RED, transportation_type=TransportationType.SHIP,
+    ),
+    Route(
+        cities=(default_cities["Hamburg"], default_cities["Moskva"]),
+        length=2, color=Color.WHITE, transportation_type=TransportationType.TRAIN,
+    ),
+    Route(
+        cities=(default_cities["Hamburg"], default_cities["Moskva"]),
+        length=2, color=Color.BLACK, transportation_type=TransportationType.TRAIN,
+    ),
+    Route(
+        cities=(default_cities["Hamburg"], default_cities["Marseille"]),
+        length=1, color=Color.PINK, transportation_type=TransportationType.TRAIN,
+    ),
+    Route(
+        cities=(default_cities["Hamburg"], default_cities["Marseille"]),
+        length=1, color=Color.RED, transportation_type=TransportationType.TRAIN,
+    ),
+    Route(
+        cities=(default_cities["Hamburg"], default_cities["Edinburgh"]),
+        length=1, color=Color.BLACK, transportation_type=TransportationType.SHIP,
+    ),
+    Route(
+        cities=(default_cities["Hamburg"], default_cities["Edinburgh"]),
+        length=1, color=Color.YELLOW, transportation_type=TransportationType.SHIP,
+    ),
+    Route(
+        cities=(default_cities["Marseille"], default_cities["Edinburgh"]),
+        length=1, color=Color.RED, transportation_type=TransportationType.SHIP,
+    ),
+    Route(
+        cities=(default_cities["Marseille"], default_cities["Edinburgh"]),
+        length=1, color=Color.GREEN, transportation_type=TransportationType.SHIP,
+    ),
 )
